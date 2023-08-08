ed233df2fd34c5b34d030f675b00b1df40bda798
Eric Nichols <ebn646@gmail.com>
2023-08-08 09:48:01 -0400

    added clerk sign in

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,5 @@
 import '#/styles/globals.css';
+import { ClerkProvider } from '@clerk/nextjs';
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
@@ -6,7 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: {
-    default: 'Next.js App Router',
+    default: 'Next.js Threads',
     template: '%s | Next.js App Router',
   },
   description:
@@ -28,8 +29,10 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   )
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,11 @@
+import { UserButton } from "@clerk/nextjs"
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Threads</h1>
+      <div>
+        <UserButton afterSignOutUrl="/" />
+      </div>
     </main>
   )
 }
