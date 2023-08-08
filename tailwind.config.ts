3541a8a469e9e6c0711cab74b23b8d43ec08eb61
Eric Nichols <ebn646@gmail.com>
2023-08-08 08:27:15 -0400

    updated files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,90 +1,216 @@
-import colors from 'tailwindcss/colors';
-import { Config } from 'tailwindcss';
-
-export default {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ['class'],
   content: [
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './ui/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
   ],
-  future: {
-    hoverOnlyWhenSupported: true,
-  },
-  darkMode: 'class',
   theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
+    fontSize: {
+      'heading1-bold': [
+        '36px',
+        {
+          lineHeight: '140%',
+          fontWeight: '700',
+        },
+      ],
+      'heading1-semibold': [
+        '36px',
+        {
+          lineHeight: '140%',
+          fontWeight: '600',
+        },
+      ],
+      'heading2-bold': [
+        '30px',
+        {
+          lineHeight: '140%',
+          fontWeight: '700',
+        },
+      ],
+      'heading2-semibold': [
+        '30px',
+        {
+          lineHeight: '140%',
+          fontWeight: '600',
+        },
+      ],
+      'heading3-bold': [
+        '24px',
+        {
+          lineHeight: '140%',
+          fontWeight: '700',
+        },
+      ],
+      'heading4-medium': [
+        '20px',
+        {
+          lineHeight: '140%',
+          fontWeight: '500',
+        },
+      ],
+      'body-bold': [
+        '18px',
+        {
+          lineHeight: '140%',
+          fontWeight: '700',
+        },
+      ],
+      'body-semibold': [
+        '18px',
+        {
+          lineHeight: '140%',
+          fontWeight: '600',
+        },
+      ],
+      'body-medium': [
+        '18px',
+        {
+          lineHeight: '140%',
+          fontWeight: '500',
+        },
+      ],
+      'body-normal': [
+        '18px',
+        {
+          lineHeight: '140%',
+          fontWeight: '400',
+        },
+      ],
+      'body1-bold': [
+        '18px',
+        {
+          lineHeight: '140%',
+          fontWeight: '700',
+        },
+      ],
+      'base-regular': [
+        '16px',
+        {
+          lineHeight: '140%',
+          fontWeight: '400',
+        },
+      ],
+      'base-medium': [
+        '16px',
+        {
+          lineHeight: '140%',
+          fontWeight: '500',
+        },
+      ],
+      'base-semibold': [
+        '16px',
+        {
+          lineHeight: '140%',
+          fontWeight: '600',
+        },
+      ],
+      'base1-semibold': [
+        '16px',
+        {
+          lineHeight: '140%',
+          fontWeight: '600',
+        },
+      ],
+      'small-regular': [
+        '14px',
+        {
+          lineHeight: '140%',
+          fontWeight: '400',
+        },
+      ],
+      'small-medium': [
+        '14px',
+        {
+          lineHeight: '140%',
+          fontWeight: '500',
+        },
+      ],
+      'small-semibold': [
+        '14px',
+        {
+          lineHeight: '140%',
+          fontWeight: '600',
+        },
+      ],
+      'subtle-medium': [
+        '12px',
+        {
+          lineHeight: '16px',
+          fontWeight: '500',
+        },
+      ],
+      'subtle-semibold': [
+        '12px',
+        {
+          lineHeight: '16px',
+          fontWeight: '600',
+        },
+      ],
+      'tiny-medium': [
+        '10px',
+        {
+          lineHeight: '140%',
+          fontWeight: '500',
+        },
+      ],
+      'x-small-semibold': [
+        '7px',
+        {
+          lineHeight: '9.318px',
+          fontWeight: '600',
+        },
+      ],
+    },
     extend: {
-      // https://vercel.com/design/color
       colors: {
-        gray: colors.zinc,
-        'gray-1000': 'rgb(17,17,19)',
-        'gray-1100': 'rgb(10,10,11)',
-        vercel: {
-          pink: '#FF0080',
-          blue: '#0070F3',
-          cyan: '#50E3C2',
-          orange: '#F5A623',
-          violet: '#7928CA',
+        'primary-500': '#877EFF',
+        'secondary-500': '#FFB620',
+        blue: '#0095F6',
+        'logout-btn': '#FF5A5A',
+        'navbar-menu': 'rgba(16, 16, 18, 0.6)',
+        'dark-1': '#000000',
+        'dark-2': '#121417',
+        'dark-3': '#101012',
+        'dark-4': '#1F1F22',
+        'light-1': '#FFFFFF',
+        'light-2': '#EFEFEF',
+        'light-3': '#7878A3',
+        'light-4': '#5C5C7B',
+        'gray-1': '#697C89',
+        glassmorphism: 'rgba(16, 16, 18, 0.60)',
+      },
+      boxShadow: {
+        'count-badge': '0px 0px 6px 2px rgba(219, 188, 159, 0.30)',
+        'groups-sidebar': '-30px 0px 60px 0px rgba(28, 28, 31, 0.50)',
+      },
+      screens: {
+        xs: '400px',
+      },
+      keyframes: {
+        'accordion-down': {
+          from: { height: 0 },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: 0 },
         },
       },
-      backgroundImage: ({ theme }) => ({
-        'vc-border-gradient': `radial-gradient(at left top, ${theme(
-          'colors.gray.500',
-        )}, 50px, ${theme('colors.gray.800')} 50%)`,
-      }),
-      keyframes: ({ theme }) => ({
-        rerender: {
-          '0%': {
-            ['border-color']: theme('colors.vercel.pink'),
-          },
-          '40%': {
-            ['border-color']: theme('colors.vercel.pink'),
-          },
-        },
-        highlight: {
-          '0%': {
-            background: theme('colors.vercel.pink'),
-            color: theme('colors.white'),
-          },
-          '40%': {
-            background: theme('colors.vercel.pink'),
-            color: theme('colors.white'),
-          },
-        },
-        loading: {
-          '0%': {
-            opacity: '.2',
-          },
-          '20%': {
-            opacity: '1',
-            transform: 'translateX(1px)',
-          },
-          to: {
-            opacity: '.2',
-          },
-        },
-        shimmer: {
-          '100%': {
-            transform: 'translateX(100%)',
-          },
-        },
-        translateXReset: {
-          '100%': {
-            transform: 'translateX(0)',
-          },
-        },
-        fadeToTransparent: {
-          '0%': {
-            opacity: '1',
-          },
-          '40%': {
-            opacity: '1',
-          },
-          '100%': {
-            opacity: '0',
-          },
-        },
-      }),
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
     },
   },
-  plugins: [require('@tailwindcss/typography'), require('@tailwindcss/forms')],
-} satisfies Config;
+  plugins: [require('tailwindcss-animate')],
+};
